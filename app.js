45ef624194cab77062146bd8f793befe32048f05
Jungmin <jungmin.yjm@gmail.com>
2016-12-14 11:48:37 -0500

    Updated DB connection code to use MongoLab when in production

--- app.js
+++ app.js
@@ -19,25 +19,13 @@ var blogsRouter = require('./routes/blogs');
 var seedDB      = require('./seeds');
 
 
-// Connect to database
-// if (process.env.MONGODB_URI) {
-//   mongoose.connect('mongodb://localhost/posts');
-// }
-// else {
-//   mongoose.connect('mongodb://localhost/express-movies');
-// }
-// mongoose.connection.on('error', function(err) {
-//   console.error('MongoDB connection error: ' + err);
-//   process.exit(-1);
-//   }
-// );
-// mongoose.connection.once('open', function() {
-//   console.log("Mongoose has connected to MongoDB!");
-// });
-
-
-// Connect to database
-mongoose.connect('mongodb://localhost/posts');
+// Connect to database -- heroku
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/posts');
+}
 mongoose.connection.on('error', function(err) {
   console.error('MongoDB connection error: ' + err);
   process.exit(-1);
@@ -47,6 +35,18 @@ mongoose.connection.once('open', function() {
   console.log("Mongoose has connected to MongoDB!");
 });
 seedDB()
+
+// Connect to database
+// mongoose.connect('mongodb://localhost/posts');
+// mongoose.connection.on('error', function(err) {
+//   console.error('MongoDB connection error: ' + err);
+//   process.exit(-1);
+//   }
+// );
+// mongoose.connection.once('open', function() {
+//   console.log("Mongoose has connected to MongoDB!");
+// });
+// seedDB()
 
 // view engin settings
 app.set('views', './views');
