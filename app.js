3003f1fccc1276474725bcccc6c9880e99bbbfce
Jungmin <jungmin.yjm@gmail.com>
2016-12-14 13:22:46 -0500

    addon added

--- app.js
+++ app.js
@@ -21,24 +21,12 @@ var seedDB      = require('./seeds');
 
 
 // Connect to database -- heroku
-// if (process.env.MONGODB_URI) {
-//   mongoose.connect(process.env.MONGODB_URI);
-// }
-// else {
-//   mongoose.connect('mongodb://localhost/posts');
-// }
-// mongoose.connection.on('error', function(err) {
-//   console.error('MongoDB connection error: ' + err);
-//   process.exit(-1);
-//   }
-// );
-// mongoose.connection.once('open', function() {
-//   console.log("Mongoose has connected to MongoDB!");
-// });
-// seedDB()
-
-// Connect to database
-mongoose.connect('mongodb://localhost/posts');
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/posts');
+}
 mongoose.connection.on('error', function(err) {
   console.error('MongoDB connection error: ' + err);
   process.exit(-1);
@@ -47,6 +35,18 @@ mongoose.connection.on('error', function(err) {
 mongoose.connection.once('open', function() {
   console.log("Mongoose has connected to MongoDB!");
 });
+// seedDB()
+
+// Connect to database
+// mongoose.connect('mongodb://localhost/posts');
+// mongoose.connection.on('error', function(err) {
+//   console.error('MongoDB connection error: ' + err);
+//   process.exit(-1);
+//   }
+// );
+// mongoose.connection.once('open', function() {
+//   console.log("Mongoose has connected to MongoDB!");
+// });
 // seedDB()
 
 // view engin settings
